5a3c0aff09ef88130e0a09732ed9e5b6f3f5dd0e
Tuan Le <lethanhtuan2897@hotmail.com>
2018-08-21 23:23:58 +0700

    #All: update gitignore

--- bin/Release/net461/publish/wwwroot/js/ckeditor.traloilienlac.config.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.editorConfig = (config) =>
-{
-    config.toolbar = [
-        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
-        { name: 'paragraph', items: ['NumberedList', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
-        { name: 'styles', items: ['Bold', 'Italic', 'Underline', 'Font', 'FontSize'] },
-    ];
-}
