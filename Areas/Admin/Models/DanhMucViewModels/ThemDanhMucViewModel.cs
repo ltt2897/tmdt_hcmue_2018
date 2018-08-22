cef3d360a9e22d2af41ff6e82ffaa8f07b5c211f
Tuan Le <lethanhtuan2897@hotmail.com>
2018-08-22 23:52:41 +0700

    #All-Tuan-7: Update danh mục

--- Areas/Admin/Models/DanhMucViewModels/ThemDanhMucViewModel.cs
+++ Areas/Admin/Models/DanhMucViewModels/ThemDanhMucViewModel.cs
@@ -3,6 +3,6 @@ namespace PhuKienDienThoai.Areas.Admin.Models.DanhMucViewmodels
     public class ThemDanhMucViewModel
     {
         public string TenDanhMuc { get; set; }
-        
+        public int MatHangId { get; set; }
     }
 }
