d6edafdffeecd81c6c6a343e91da937572db3d7a
Tuan Le <lethanhtuan2897@hotmail.com>
2018-08-24 23:31:49 +0700

    #All-Tuan-10

--- PhuKienDienThoai/Controllers/PaymentController.cs
+++ PhuKienDienThoai/Controllers/PaymentController.cs
@@ -7,6 +7,11 @@ using Newtonsoft.Json;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using PhuKienDienThoai.Configurations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PhuKienDienThoai.Controllers
 {
@@ -14,9 +19,16 @@ namespace PhuKienDienThoai.Controllers
     {
         private IPaypalServices _PaypalServices;
 
-        public PaymentController(IPaypalServices paypalServices)
+        ApplicationDbContext context;
+        UserManager<ApplicationUser> usermanager;
+        IHostingEnvironment environment;
+
+        public PaymentController(IPaypalServices paypalServices, ApplicationDbContext _c, UserManager<ApplicationUser> _usermanager, IHostingEnvironment _env)
         {
             _PaypalServices = paypalServices;
+            context = _c;
+            usermanager = _usermanager;
+            environment = _env;
         }
 
         public IActionResult Index()
@@ -39,19 +51,60 @@ namespace PhuKienDienThoai.Controllers
         public IActionResult ExecutePayment(string paymentId, string PayerID)
         {
             var payment = _PaypalServices.ExecutePayment(paymentId, PayerID);
-
+            
             // Hint: You can save the transaction details to your database using payment/buyer info
             return Ok();
         }
 
-        public IActionResult Success()
+        [HttpGet]
+        [Authorize(Roles = "Admin, User")]
+        public async Task<IActionResult> Success(string DiaChi, string GhiChu)
         {
-            return View();
-        }
+            //lấy dữ liệu từ session
+            var stringItem = HttpContext.Session.GetString("GioHang");
+            var currentUser = await usermanager.GetUserAsync(User);
+            var ListItemTrongGioHang = new List<GioHangViewModel>();
+            try
+            {
+                if (!string.IsNullOrEmpty(stringItem))
+                {
+                    ListItemTrongGioHang = JsonConvert.DeserializeObject<List<GioHangViewModel>>(stringItem);
+                    var ChiTietHoaDon = new List<Models.ChiTietHoaDon>();
 
-        public IActionResult Cancel()
-        {
-            return View();
+                    foreach (var item in ListItemTrongGioHang)
+                    {
+                        ChiTietHoaDon.Add(new Models.ChiTietHoaDon
+                        {
+                            SanPhamId = item.SanPham.id,
+                            SoLuong = item.SoLuong,
+                            ThanhTien = item.SanPham.DonGia * item.SoLuong,
+                        });
+                    }
+
+                    var HoaDon = await context.HoaDon.AddAsync(new Models.HoaDon
+                    {
+                        ChiTietHoaDons = ChiTietHoaDon,
+                        DiaChi = WebUtility.UrlDecode(DiaChi),
+                        GhiChu = WebUtility.UrlDecode(GhiChu),
+                        PhuongThucThanhToan = "Đã thanh toán qua PayPal",
+                        TongThanhTien = ChiTietHoaDon.Sum(x => x.ThanhTien),
+                        User = currentUser,
+                    });
+                    await context.SaveChangesAsync();
+                    
+                    TempData["Message"] = "Đặt hàng thành công! Cảm ơn quý khách đã tin tưởng chúng tôi";
+                    HttpContext.Session.Clear();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Logs.Logger.Log(ex.ToString());
+            }
+
+            return RedirectToAction(
+                actionName: "Index",
+                controllerName: "Home"
+            );
         }
 
     }
